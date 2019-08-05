1a97cab486dc711bfa8ce8456d083b9852c6586b
Maro Hdz <marohdzg@gmail.com>
2019-08-05 15:47:21 -0500

    Se agregan las clases de las tablas.

---
+++ iS_Repair/iS_Repair/Clases/ClasesTablas/Pedido.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iS_Repair.Clases.ClasesTablas
+{
+    class Pedido
+    {
+        private string _strIdCliente;
+
+        public string IdCliente
+        {
+            get { return _strIdCliente; }
+            set { _strIdCliente = value; }
+        }
+
+        private string _strPieza;
+
+        public string Pieza
+        {
+            get { return _strPieza; }
+            set { _strPieza = value; }
+        }
+
+        private double _dblCosto;
+
+        public double Costo
+        {
+            get { return _dblCosto; }
+            set { _dblCosto = value; }
+        }
+
+        private bool _blnPedido;
+
+        public bool EsPedido
+        {
+            get { return _blnPedido; }
+            set { _blnPedido = value; }
+        }
+
+        private DateTime _dtmFechaPedido;
+
+        public DateTime FechaPedido
+        {
+            get { return _dtmFechaPedido; }
+            set { _dtmFechaPedido = value; }
+        }
+
+        private DateTime _dtmFechaRegistro;
+
+        public DateTime FechaRegistro
+        {
+            get { return _dtmFechaRegistro; }
+            set { _dtmFechaRegistro = value; }
+        }
+    }
+}
