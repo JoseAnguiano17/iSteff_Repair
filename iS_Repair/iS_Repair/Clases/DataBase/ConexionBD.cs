46df0ac7f91f6c751a1a8207aebe401cf1e6deed
JoseAnguiano17 <44876080+JoseAnguiano17@users.noreply.github.com>
2019-12-06 02:42:39 -0600

    Login (#27)
    * Login y cambios a clases Empleado y ConexionBD
    
    * Revert "Login y cambios a clases Empleado y ConexionBD"
    
    This reverts commit 95e72012c21c7bab13944d2fd2bbac3420b328d5.
    
    * Login

--- iS_Repair/iS_Repair/Clases/DataBase/ConexionBD.cs
+++ iS_Repair/iS_Repair/Clases/DataBase/ConexionBD.cs
@@ -94,7 +94,7 @@ namespace iS_Repair.Clases.DataBase
                 SqlDataReader Empleados = comando.ExecuteReader();
                 while (Empleados.Read())
                 {
-                    listaEmpleados.Add(new Empleado(Empleados.GetString(0), Empleados.GetString(1), Empleados.GetString(2), Empleados.GetString(3), Empleados.GetString(4), Empleados.GetString(5), Empleados.GetString(6), Empleados.GetString(7)));
+                    listaEmpleados.Add(new Empleado(Empleados.GetString(0), Empleados.GetString(1), Empleados.GetString(2), Empleados.GetString(3), Empleados.GetString(4), Empleados.GetString(5), Empleados.GetString(6), Empleados.GetString(7), Empleados.GetBoolean(8)));
                 }
                 Empleados.Close();
                 con.Close();
@@ -109,7 +109,7 @@ namespace iS_Repair.Clases.DataBase
                 SqlCommand comando = new SqlCommand("INSERT INTO EMPLEADO (USUARIO, CONTRASENA, NOMBRE, APELLIDOP" +
                                                     " APELLIDOM, NUMTELEFONO, CORREO, DIRECCION)" +
                                          " VALUES (@usuario, @contrasena, @nombre, @apellidoP, @apellidom, @numtelefono" +
-                                         " @correo, @direccion)", con);
+                                         " @correo, @direccion, @administrador)", con);
                 comando.Parameters.AddWithValue("@usuario", emp.Usuario);
                 comando.Parameters.AddWithValue("@contrasena", emp.Contrasena);
                 comando.Parameters.AddWithValue("@nombre", emp.Nombre);
@@ -118,6 +118,7 @@ namespace iS_Repair.Clases.DataBase
                 comando.Parameters.AddWithValue("@numtelefono", emp.Telefono);
                 comando.Parameters.AddWithValue("@correo", emp.Correo);
                 comando.Parameters.AddWithValue("@direccion", emp.Direccion);
+                comando.Parameters.AddWithValue("@administrador", emp.Administrador);
                 comando.ExecuteNonQuery();
                 con.Close();
             }
@@ -347,6 +348,38 @@ namespace iS_Repair.Clases.DataBase
                 con.Close();
             }
             return intID;
+        }
+
+        public static Empleado DetectarAdmin()
+        {
+            Empleado empleado = new Empleado();
+            using (SqlConnection con = ObtenerConexion())
+            {
+                SqlCommand comando = new SqlCommand("SELECT * FROM EMPLEADO WHERE ADMINISTRADOR = 1", con);
+                SqlDataReader sqlEmpleado = comando.ExecuteReader();
+                while (sqlEmpleado.Read())
+                {
+                    empleado = new Empleado(sqlEmpleado.GetString(0), sqlEmpleado.GetString(1), sqlEmpleado.GetString(2), sqlEmpleado.GetString(3), sqlEmpleado.GetString(4), sqlEmpleado.GetString(5), sqlEmpleado.GetString(6), sqlEmpleado.GetString(7), sqlEmpleado.GetBoolean(8));
+                }
+                sqlEmpleado.Close();
+            }
+            return empleado;
+        }
+
+        public static Empleado DetectarEmpleado(string strUsuario, string strContrasena)
+        {
+            Empleado empleado = new Empleado();
+            using (SqlConnection con = ObtenerConexion())
+            {
+                SqlCommand comando = new SqlCommand("SELECT * FROM EMPLEADO WHERE USUARIO = '" + strUsuario + "' AND CONTRASENA = '" + strContrasena + "'", con);
+                SqlDataReader sqlEmpleado = comando.ExecuteReader();
+                while (sqlEmpleado.Read())
+                {
+                    empleado = new Empleado(sqlEmpleado.GetString(0), sqlEmpleado.GetString(1), sqlEmpleado.GetString(2), sqlEmpleado.GetString(3), sqlEmpleado.GetString(4), sqlEmpleado.GetString(5), sqlEmpleado.GetString(6), sqlEmpleado.GetString(7), sqlEmpleado.GetBoolean(8));
+                }
+                sqlEmpleado.Close();
+            }
+            return empleado;
         }
     }
 }
